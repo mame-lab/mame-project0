c82ea5015ffb854250485780feb0db9e1b11747f
mame-lab <penda.mpn@gmail.com>
2019-10-06 19:01:36 -0500

    First commit

---
+++ ManaStyle.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ManaStyle.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
